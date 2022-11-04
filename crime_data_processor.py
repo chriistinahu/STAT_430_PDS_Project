97b2b65a67ae2cbda81eb5cca781be104ef1ccad
Christina Hu <ch35@illinois.edu>
2022-11-03 23:03:30 -0500

    able to retrieve data through API

--- crime_data_processor.py
+++ crime_data_processor.py
@@ -1,6 +1,7 @@
 # API key ID: otlwwfxero72fkms03hi49x2
 # API key secret: 5qbnpzqphl1y8bz6eggxraxlzlnbnhso49bro4925r1rpxj6sn
-# app token
+# app token: 8y23ArYxg4pcvOMgWzMFSC0nB
+# secret app token: pZgwEL4lKm2r1s868Saz3EJ1ebpTKy-FzglU
 
 # crime data docs: https://dev.socrata.com/foundry/data.cityofchicago.org/9hwr-2zxp
 
@@ -12,10 +13,16 @@ import pandas as pd
 import sodapy
 from sodapy import Socrata
 
-crime_url = "https://data.cityofchicago.org/resource/9hwr-2zxp.json"
-# Unauthenticated client only works with public data sets. Note 'None'
-# in place of application token, and no username or password:
-client = Socrata("data.cityofchicago.org", None)
+# crime_url = "https://data.cityofchicago.org/resource/9hwr-2zxp.json"
+
+
+app_token = "8y23ArYxg4pcvOMgWzMFSC0nB"
+
+# Example authenticated client (needed for non-public datasets):
+client = Socrata("data.cityofchicago.org",
+                 app_token, 
+                 username="ch35@illinois.edu",
+                 password="68M8rz3vF!StBjD")
 
 
 # First 2000 results, returned as JSON from API / converted to Python list of
@@ -25,6 +32,5 @@ results = client.get("9hwr-2zxp", limit=10000)
 # Convert to pandas DataFrame
 crime_df = pd.DataFrame.from_records(results)
 
-print(crime_df)
+print(crime_df.shape)
 
-# THIS DOES NOT WORK YET. I AM CONTACTING SUPPORT BC I HAVE NO IDEA HOW TO GET AN APP TOKEN THAT WE NEED TO MAKE THE API CALLS WITH.
